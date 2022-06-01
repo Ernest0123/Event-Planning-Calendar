e13ef9631e219657cbaf64e1dc33ef210c2ccf67
pdcproj <105491843+pdcproj@users.noreply.github.com>
2022-06-01 09:01:09 +0000

    Wed, Jun 1, 2022, 9:01 AM +00:00

--- WDC_Project/app.js
+++ WDC_Project/app.js
@@ -13,10 +13,35 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+// setup connection pool, after app
+var dbConnectionPool = mysql.createPool( {host: 'localhost', database: 'adminAccount' });
+
+// order does matter!!!
+app.use(function(req, res, next) {
+
+    // making "dbConnectionPool available in req.pool
+    req.pool = dbConnectionPool;
+
+    // pass the request on
+    next();
+});
+
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// the session code
+app.use(session({
+    //           //
+    secret: 'kjdsfkjgfkjhkfjghkahjfg',          //           //
+    resave: false,                              // THIS CODE //
+    saveUninitialized: true,                    //           //
+    cookie: { secure: false }                   //           //
+}));
+
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
