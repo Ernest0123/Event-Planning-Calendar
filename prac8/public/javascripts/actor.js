c6e0a0b77ff7ce428a46a83c6711053725dbe263
pdcproj <105491843+pdcproj@users.noreply.github.com>
2022-06-08 12:51:49 +0000

    Wed, Jun 8, 2022, 12:51 PM +00:00

---
+++ prac8/public/javascripts/actor.js
@@ -0,0 +1,44 @@
+var actor_initial = [];
+function show_actor(){
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+            let d = '';
+            if (this.readyState == 4 && this.status == 200) {
+                actor_initial = JSON.parse(this.responseText);
+                for(i=0; i<actor_initial.length; i++)
+                {
+                    d = d +`<tr>
+                    <td>${actor_initial[i].first_name}</td>
+                    <td>${actor_initial[i].last_name}</td>
+                    </tr>`;
+                }
+             document.getElementById("actorsshow").innerHTML = d;
+            }
+        };
+    xmlhttp.open("GET", "/users/actor", true);
+    xmlhttp.setRequestHeader("Content-type", "application/json");
+    xmlhttp.send();
+
+}
+
+
+function actor_add(){
+    var firstname = document.getElementById('actor-first-name').value;
+    var lastname = document.getElementById('actor-last-name').value;
+    //console.log(firstname, lastname);
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+        actor_initial = JSON.parse(this.responseText);
+
+       }
+
+    };
+
+    var data={"firstname":firstname,"lastname":lastname};
+    //console.log(data);
+    xmlhttp.open("POST", "/users/addactor", true);
+    xmlhttp.setRequestHeader("Content-type", "application/json");
+    xmlhttp.send(JSON.stringify(data));
+
+}
---
+++ prac8/routes/users.js
@@ -0,0 +1,59 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+/* Task2-1 */
+router.get('/actor', function(req, res, next){
+  req.pool.getConnection( function(error,connection){
+      if(error) {
+          res.sendStatus(500);
+          return;
+      }
+      //squery to get all the last name and last name from actor
+      var query = `SELECT first_name, last_name FROM actor;`;
+      connection.query(query, function(error, rows, fields){
+          connection.release();
+          if(error) {
+              res.sendStatus(500);
+              return;
+          }
+          res.json(rows);
+      });
+  });
+});
+
+router.post('/addactor',function(req,res,next){
+
+  var firstname=req.body.firstname;
+  var lastname=req.body.lastname;
+  req.pool.getConnection(function(error,connection){
+    if(error) {
+      res.sendStatus(500);
+      return;
+  }
+  var query = "INSERT INTO actor(first_name, last_name) VALUES(?, ?); ";
+  value=[firstname,lastname];
+  connection.query(query,value,
+    function(error, rows, fields){
+    connection.release();
+    if(error) {
+        res.sendStatus(500);
+        return;
+    }
+    res.json(rows);
+  });
+
+  });
+
+});
+
+
+
+
+
+
+module.exports = router;
