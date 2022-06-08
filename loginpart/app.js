f580bf883187ed39f6e9640a9622ac4a18fb63e3
pdcproj <105491843+pdcproj@users.noreply.github.com>
2022-06-08 02:54:18 +0000

    Wed, Jun 8, 2022, 2:54 AM +00:00

--- loginpart/app.js
+++ loginpart/app.js
@@ -27,13 +27,14 @@ app.use(function(req, res, next) {
     next();
 });
 
+app.use(logger('dev'));
+app.use(express.json());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(logger('dev'));
-app.use(express.json());
+
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
