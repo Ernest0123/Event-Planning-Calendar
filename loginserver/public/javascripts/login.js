330f8c6290e2c523b8ccb0af19a95e386e59bdf9
pdcproj <105491843+pdcproj@users.noreply.github.com>
2022-06-08 09:39:17 +0000

    Wed, Jun 8, 2022, 9:39 AM +00:00

--- loginserver/public/javascripts/login.js
+++ loginserver/public/javascripts/login.js
@@ -64,7 +64,7 @@ function login() {
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             alert("Login Successful");
-            document.getElementById("usrname").value = user.username;
+            document.getElementById("usrname").innerHTML = user.username;
         } else if (this.readyState == 4 && this.status >= 400) {
             alert("Login Failed");
         }
