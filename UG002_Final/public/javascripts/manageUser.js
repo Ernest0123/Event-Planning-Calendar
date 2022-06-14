1ee5c7225ff8a336890d6d46a254ea43efedf07f
pdcproj <105491843+pdcproj@users.noreply.github.com>
2022-06-14 07:09:40 +0000

    Tue, Jun 14, 2022, 7:09 AM +00:00

---
+++ UG002_Final/public/javascripts/manageUser.js
@@ -0,0 +1,25 @@
+function checklogin() {
+
+    var xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function (){
+        if(this.readyState == 4 && this.status >= 200 && this.status < 400){
+            //alert("You are logged in.");
+            var userdata = JSON.parse(this.responseText);
+            document.getElementById("usernamedisplay").innerHTML = userdata[0].username;
+            document.getElementById("firstnamedisplay").innerHTML = userdata[0].firstname;
+            document.getElementById("lastnamedisplay").innerHTML = userdata[0].lastname;
+            document.getElementById("emaildisplay").innerHTML = userdata[0].email;
+        } else if (this.readyState == 4 && this.status >= 400) {
+            alert("You are not logged in.");
+            window.location.replace("/main.html");
+        }
+    };
+    xhttp.open("POST", "/getusrinfo",true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+    xhttp.send();
+}
+
+function gobacktomain(){
+    window.location.replace("/main.html");
+}
